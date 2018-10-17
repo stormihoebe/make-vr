45984d8646d716ec9bf25e03dc865b25fcee6ece
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2018-10-16 17:18:35 -0700

    start

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var yeoman = require('yeoman-environment');
 var env = yeoman.createEnv();
 
-module.exports = env.lookup(function () {
-    console.log('Welcome to make vr!')
-    console.log(`Let's set up a WebVR project using A-Frame and Typescript`)
+export default start = () => {
+    env.lookup( 
+        () =>   {
+            console.log('Welcome to make vr!')
+            console.log(`Let's set up a WebVR project using A-Frame and Typescript`)
 
-    env.run('make-ts-vr');
-});
+            env.run('make-ts-vr')
+        }
+    )
+}
